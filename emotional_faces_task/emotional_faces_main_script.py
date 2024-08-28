b7fdf488544f7dd430e31d5318d089372450e096
samwray746 <S.Wray@bsms.ac.uk>
2024-08-28 13:31:04 +0100

    Update emotional_faces_main_script.py

--- emotional_faces_task/emotional_faces_main_script.py
+++ emotional_faces_task/emotional_faces_main_script.py
@@ -54,20 +54,62 @@ win.mouseVisible = False
 
 ### FUNCTION TO CREATE SOME STIMULI ### 
 
-# n_1 and d_1 correspond to objects of the neutral and disgust image of 'actor 1' in 
+# Note: n_1 and d_1 correspond to objects of the neutral and disgust image of 'actor 1', and so on with n_2, d_2...etc. 
 
 ## Function needs to be written - remove comment when done ##
 n_1, d_1, n_2, d_2, n_3, d_3, n_4, d_4, n_5, d_5, n_6, d_6, n_7, d_7, n_8, d_8, n_9, d_9, n_10, d_10, n_11, d_11, n_12, d_12, n_13, d_13, n_14, d_14, n_15, d_15, n_16, d_16, n_17, d_17, n_18, d_18, n_19, d_19, n_20, d_20, n_21, d_21, n_22, d_22, n_23, d_23, n_24, d_24, n_25, d_25, n_26, d_26, n_27, d_27, n_28, d_28, n_29, d_29, n_30, d_30, n_31, d_31, n_32, d_32, n_33, d_33, n_34, d_34, n_35, d_35, n_36, d_36, n_37, d_37, n_38, d_38, n_39, d_39, n_40, d_40 = experimental_stimuli(win)
 
+### INSTRUCTIONS ### 
+
+## Function needs to be written - remove comment when done ## 
+instructions(win, 'all_instruction_variables')
+
 ### EXPERIMENTAL BLOCK FUNCTIONS ### 
 ## Functions need to be written - remove comment when done ## 
 if order == 1: # experimental blocks run in order 'cue_useful', 'cue_sometimes_useful', 'cue_useless'
-    eb_useful(all_relevant_variables)
-    save_experimental_block(all_relevant_variables)
+    eb_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
 
-    eb_useful(all_relevant_variables)
-    save_experimental_block(all_relevant_variables)
+    eb_s_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
 
-if order == 2: # 
+    eb_useless('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+if order == 2: # experimental blocks run in order 'cue_useful', 'cue_useless', 'cue_sometimes_useful' 
+    eb_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+    eb_useless('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+    eb_s_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+if order == 3: # experimental blocks run in order 'cue_sometimes_useful', 'cue_useless', 'cue_useful'
+    eb_s_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+    eb_s_useless('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+    eb_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+if order == 4: # experimental blocks run in order 'cue_sometimes_useful','cue_useful', 'cue_useless'
+    eb_s_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+    eb_s_useful('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+    eb_useless('all_relevant_variables')
+    save_experimental_block('all_relevant_variables')
+
+
+
+
+    
+    
     
 
