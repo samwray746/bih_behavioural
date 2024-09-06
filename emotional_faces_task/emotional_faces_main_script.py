ad88fb326d6c8da4532c0e9ddf9d6739d54f4126
Magdalena Pfaff <155568549+magdapf@users.noreply.github.com>
2024-09-06 12:34:13 +0100

    Update emotional_faces_main_script.py

--- emotional_faces_task/emotional_faces_main_script.py
+++ emotional_faces_task/emotional_faces_main_script.py
@@ -55,7 +55,7 @@ win.mouseVisible = False
 ### FUNCTION TO CREATE SOME STIMULI ### 
 
 # Note: n_1 and d_1 correspond to objects of the neutral and disgust image of 'actor 1', and so on with n_2, d_2...etc. 
-n_1, d_1, n_2, d_2, n_3, d_3, n_4, d_4, n_5, d_5, n_6, d_6, n_7, d_7, n_8, d_8, n_9, d_9, n_10, d_10, n_11, d_11, n_12, d_12, n_13, d_13, n_14, d_14, n_15, d_15, n_16, d_16, n_17, d_17, n_18, d_18, n_19, d_19, n_20, d_20, n_21, d_21, n_22, d_22, n_23, d_23, n_24, d_24, n_25, d_25, n_26, d_26, n_27, d_27, n_28, d_28, n_29, d_29, n_30, d_30, n_31, d_31, n_32, d_32, n_33, d_33, n_34, d_34, n_35, d_35, n_36, d_36, n_37, d_37, n_38, d_38, n_39, d_39, n_40, d_40, instructions_one, fixation_cross_red, fixation_cross_white, left_arrow, right_arrow = create_stimuli(win)
+n_1, d_1, n_2, d_2, n_3, d_3, n_4, d_4, n_5, d_5, n_6, d_6, n_7, d_7, n_8, d_8, n_9, d_9, n_10, d_10, n_11, d_11, n_12, d_12, n_13, d_13, n_14, d_14, n_15, d_15, n_16, d_16, n_17, d_17, n_18, d_18, n_19, d_19, n_20, d_20, n_21, d_21, n_22, d_22, n_23, d_23, n_24, d_24, n_25, d_25, n_26, d_26, n_27, d_27, n_28, d_28, n_29, d_29, n_30, d_30, n_31, d_31, n_32, d_32, n_33, d_33, n_34, d_34, n_35, d_35, n_36, d_36, n_37, d_37, n_38, d_38, n_39, d_39, n_40, d_40, instructions_one, instructions_two, instructions_three, instructions_four, fixation_cross_red, fixation_cross_white, arrow_left, arrow_right = create_stimuli(win)
 
 ### INSTRUCTIONS ### 
 # Instructions
