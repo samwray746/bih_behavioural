5a92e75b6b6399c955951e0b39a9e94f29a7c9f9
samwray746 <S.Wray@bsms.ac.uk>
2024-09-30 09:59:52 +0100

    Update create_stimuli_function.py

--- emotional_faces_task/create_stimuli_function.py
+++ emotional_faces_task/create_stimuli_function.py
@@ -5,18 +5,22 @@ def create_stimuli(win):
   
   
   ### INSTRUCTIONS
-   instruction_one_str = "Welcome to this part of the experiment! Click the space bar to continue."
-   instruction_two_str = "In this experiment, you will be shown different faces. Please pay attention to the faces"
-   instruction_three_str = "Every few trials, you will see an arrow pointin either left or right. Please indicate which way the arrow is pointing by pressing the arrrow buttons."
-   instruction_four_str = "If you have any questions, please ask the researcher before continuing. Click the space bar to start the experiment."
+   instruction_one_str = "Welcome to the emotional faces task! Press the space bar to continue."
+   instruction_two_str = "In each trial of this task, you will be shown two pictures of the same person. Pay attention to the faces."
+   instruction_three_str = "Every few trials, an arrow will appear in front of a face.\n\n\n Please indicate which way the arrow is pointing by pressing the left/right arrow keys."
+   instructions_four_str = "There are 21 experimental blocks in total, which each contain 40 trials.\n\n\nYou can take a break in-between blocks, then press spacebar to begin the next block."
+   instruction_five_str = "If you have any questions, please ask the researcher before continuing.\n\n\n Click the space bar to start the experiment."
 
    instructions_one = psychopy.visual.TextStim(win, text = instruction_one_str, height = 0.80)
    instructions_two = psychopy.visual.TextStim(win, text = instruction_two_str, height = 0.80)
    instructions_three = psychopy.visual.TextStim(win, text = instruction_three_str, height = 0.80)
    instructions_four = psychopy.visual.TextStim(win, text = instruction_four_str, height = 0.80)
+   instructions_five = psychopy.visual.TextStim(win, text = instruction_five_str, height = 0.80)
 
-   ### Colours ###
-  grey = '#808080'
+  
+
+  ### Colours ###
+   grey = '#808080'
    
   
   ### FIXATION CROSS WHITE 
@@ -72,23 +76,14 @@ def create_stimuli(win):
 
   ### LEFT POINTING RED ARROW
    arrow_left = visual.TextStim(win, text='<', color='red', height=0.2) # not sure about height?
-   arrow_left.draw()
-   win.flip()
-   core.wait(2) # how long should the arrow show? 
-   win.close() # not sure we need to close the window? 
 
  ### RIGHT POINTING RED ARROW 
    arrow_right = visual.TextStim(win, text='>', color='red', height=0.2) # not sure about height?
-   arrow_right.draw()
-   win.flip()
-   core.wait(2) # how long should the arrow show? 
-   win.close() # not sure we need to close the window? 
-
-
+ 
   ### Images - path to where the images will be saved
-###TO BE ADDED 
+   ###TO BE ADDED 
    
-   return instructions_one, instructions_two, instructions_three, instructions_four, arrow_left, arrow_right, fixation_cross_vertical_white, fixation_cross_horizontal_white, fixation_cross_vertical_red, fixation_cross_horizontal_red
+   return instructions_one, instructions_two, instructions_three, instructions_four, instructions_five, arrow_left, arrow_right, fixation_cross_vertical_white, fixation_cross_horizontal_white, fixation_cross_vertical_red, fixation_cross_horizontal_red
 
 
 
