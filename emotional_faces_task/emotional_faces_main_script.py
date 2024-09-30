a122a5e8aa9229658a2dfaadd6dcdebffb6c5ee6
samwray746 <S.Wray@bsms.ac.uk>
2024-09-30 10:16:22 +0100

    Update emotional_faces_main_script.py

--- emotional_faces_task/emotional_faces_main_script.py
+++ emotional_faces_task/emotional_faces_main_script.py
@@ -58,21 +58,7 @@ win.mouseVisible = False
 n_1, d_1, n_2, d_2, n_3, d_3, n_4, d_4, n_5, d_5, n_6, d_6, n_7, d_7, n_8, d_8, n_9, d_9, n_10, d_10, n_11, d_11, n_12, d_12, n_13, d_13, n_14, d_14, n_15, d_15, n_16, d_16, n_17, d_17, n_18, d_18, n_19, d_19, n_20, d_20, n_21, d_21, n_22, d_22, n_23, d_23, n_24, d_24, n_25, d_25, n_26, d_26, n_27, d_27, n_28, d_28, n_29, d_29, n_30, d_30, n_31, d_31, n_32, d_32, n_33, d_33, n_34, d_34, n_35, d_35, n_36, d_36, n_37, d_37, n_38, d_38, n_39, d_39, n_40, d_40, instructions_one, instructions_two, instructions_three, instructions_four, instructions_five, fixation_cross_red, fixation_cross_white, arrow_left, arrow_right = create_stimuli(win)
 
 ### INSTRUCTIONS ### 
-# Instructions
-instruction_one.draw()
-win.flip()
-
-instruction_two.draw()
-win.flip()
-
-instruction_three.draw()
-win.flip()
-
-instruction_four.draw()
-win.flip()
-
-## Function needs to be written - remove comment when done ## 
-instructions(win, 'all_instruction_variables')
+instructions(win, instructions_one, instructions_two, instructions_three, instructions_four, instructions_five)
 
 ### EXPERIMENTAL BLOCK FUNCTIONS ### 
 ## Functions need to be written - remove comment when done ## 
