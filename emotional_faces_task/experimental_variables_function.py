ede0bfd8e804ca5f5fe9390efc7d2f4113194ed1
samwray746 <S.Wray@bsms.ac.uk>
2024-09-30 11:41:54 +0100

    Create experimental_variables_function.py

---
+++ emotional_faces_task/experimental_variables_function.py
@@ -0,0 +1,19 @@
+from psychopy import core
+
+def experimental_variables(win):
+    experiment_clock = core.MonotonicClock() # the main clock used in the experiment 
+    countdown = core.CountdownTimer()
+    
+    fr = win.getActualFrameRate() # getting frame rate of the monitor 
+    one_frame = 1/fr
+
+    blocks_per_condition = 7
+    trials_per_block = 40 
+  
+    iti = [1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5] # inter-trial interval
+    isi = 0.5
+    stim_pres = 0.5
+  
+
+  
+  
