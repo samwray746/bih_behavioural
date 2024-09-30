0ac48087ab2cf6884be2fb76c0bf89bac6f9192f
samwray746 <S.Wray@bsms.ac.uk>
2024-09-30 13:12:50 +0100

    Update save_file_modification_function.py

--- emotional_faces_task/save_file_modification_function.py
+++ emotional_faces_task/save_file_modification_function.py
@@ -4,12 +4,19 @@ import os.path as op
 def save_file_modification(subject_id):
     #data_path = 'data path here' 
     subject_id_file_name = subject_id + '_emotional_faces_data.xlsx'
+    subject_id_metafile_name = subject_id + '_emotional_faces_meta_data.xlsx'
+    subject_id_training_name = subject_id + '_emotional_faces_training_data.xlsx'
     save_file = op.join(data_path, subject_id_file_name)
 
     data_wb = openpyxl.Workbook() # opening the excel file now 
+    meta_data_wb = openpyxl.Workbook()
+    training_data_wb = openpyxl.Workbook()
 
-    #faces_strings = ['block_1', 'block_2', 'block_3'....]
-    #variable_strings = [example: type of trial, arrow present?, actor, emotional face 1, emotional face 2]
+    ### Adding to main data workbook first ### 
+    block_number_list = [str(i+1) for i in range(21)] # list of strings from 1 to 5 
+    block_number_strings = [('block_' + block_number_list[i]) for i in range(len(block_number_list))] # list of blocks
+    
+    variable_strings = ['condition', 'cue_present', 'type_of_trial', 'stimulus_one', 'stimulus_2', 'systole_diastole', 'catch_trial', 'arrow_direction', 'correct_incorrect_late', 'iti']
 
     for ws_name in range(len(faces_strings)):
         data_wb.create_sheet(faces_strings[ws_name]) # creating a sheet to save each block 
