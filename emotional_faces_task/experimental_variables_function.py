5dd2d69351ddeb7a979b242523c95f8cb1222dc9
samwray746 <S.Wray@bsms.ac.uk>
2024-10-01 10:36:27 +0100

    Update experimental_variables_function.py

--- emotional_faces_task/experimental_variables_function.py
+++ emotional_faces_task/experimental_variables_function.py
@@ -8,13 +8,14 @@ def experimental_variables(win):
     one_frame = 1/fr
 
     blocks_per_condition = 7
+    total_blocks = blocks_per_condition*3
     trials_per_block = 40 
   
     iti = [1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5] # inter-trial interval
     isi = 0.5
     stim_pres = 0.5
 
-    return experiment_clock, countdown, fr, one_frame, blocks_per_condition, trials_per_block, iti, isi, stim_pres
+    return experiment_clock, countdown, fr, one_frame, blocks_per_condition, total_blocks, trials_per_block, iti, isi, stim_pres
   
 
   
