78672a47f372e8c0d78997b4e1aa11d64f84ed35
samwray746 <S.Wray@bsms.ac.uk>
2024-10-02 15:23:45 +0100

    General changes

--- emotional_faces_task/eb_useful.py
+++ emotional_faces_task/eb_useful.py
@@ -1,9 +1,56 @@
-# Function for the cue-useful experimental block 
+# Function for the cue-useful experimental block
 
-def experimental_block(win, fch_w, fcv_w, fch_nw, fcv_nw, isi, iti, arrow_left, arrow_right, n_1, d_1, n_2, d_2, n_3, d_3, n_4, d_4, n_5, d_5, n_6, d_6, n_7, d_7, n_8, d_8, n_9, d_9, n_10, d_10, n_11, d_11, n_12, d_12, n_13, d_13, n_14, d_14, n_15, d_15, n_16, d_16, n_17, d_17, n_18, d_18, n_19, d_19, n_20, d_20, n_21, d_21, n_22, d_22, n_23, d_23, n_24, d_24, n_25, d_25, n_26, d_26, n_27, d_27, n_28, d_28, n_29, d_29, n_30, d_30, n_31, d_31, n_32, d_32, n_33, d_33, n_34, d_34, n_35, d_35, n_36, d_36, n_37, d_37, n_38, d_38, n_39, d_39, n_40, d_40):
+import random
+
+def eb_useful(win, fch_w, fcv_w, fch_nw, fcv_nw, isi, iti, arrow_left, arrow_right, n_1, d_1, n_2, d_2, n_3, d_3, n_4, d_4, n_5, d_5, n_6, d_6, n_7, d_7, n_8, d_8, n_9, d_9, n_10, d_10, n_11, d_11, n_12, d_12, n_13, d_13, n_14, d_14, n_15, d_15, n_16, d_16, n_17, d_17, n_18, d_18, n_19, d_19, n_20, d_20, n_21, d_21, n_22, d_22, n_23, d_23, n_24, d_24, n_25, d_25, n_26, d_26, n_27, d_27, n_28, d_28, n_29, d_29, n_30, d_30, n_31, d_31, n_32, d_32, n_33, d_33, n_34, d_34, n_35, d_35, n_36, d_36, n_37, d_37, n_38, d_38, n_39, d_39, n_40, d_40):
     condition_record = [] # a record that this is a cue_useful trial
     cue_present = [] # was the cue present this trial? 
-    type_of_trial = [] # we know that it will be a repetition trial, but just adding for consistency 
+    type_of_trial = [] # we know that if the cue is present it will be a repetition trial as this is a cue-useful block, but just adding for consistency 
     stimulus_one = [] # the first stimulus presented in the trial 
+    stimulus_two = [] # the second stimulus presented in the trial 
+    catch = [] # was the trial a catch trial? 
+    arrow_direction = [] # if it was a catch trial, what direction was the arrow pointing? 
+    correct_incorrect = [] # was the response on the catch trial correct or incorrect 
+    iti = [] # what was the inter-trial interval for the trial? 
+    
+    # Setting up the structure of the experimental block. Some details: 
+        
+    # 1) Each block consists of 40 trials i.e., the same number of actors we have in our stimulus set
+    # 2) In each block, there are 20 repetition trials and 20 alternation trials
+    # 3) In half of the 20 repetition/alternation trials a disgust face will be presented first, and in the other half a neutral face will be presented 
+    # 4) In a cue useful block, whenever it's a repetition trial the fixation cross will always be non-white 
+    
+    
+    diff_poss_trials = ['disgust_r', 'disgust_a', 'neutral_r', 'neutral_a']
+    
+    block_structure = [] 
+    
+    for trial in range(40): # creating the structure of each experimental block 
+        
+        if len(diff_poss_trials)== 0:
+            diff_poss_trials = ['disgust_r', 'disgust_a', 'neutral_r', 'neutral_a']
+        
+        
+        trial_pick = random.choice(diff_poss_trials)
+        diff_poss_trials.remove(trial_pick)
+        block_structure.append(trial_pick)
+        
+    # appending the lists with relevant info 
+    for trial in range(40):
+        
+        condition_record.append('cue_useful')
+        if block_structure[trial] == 'disgust_r': #So a repetition trial, and disgust is shown first
+            condition_record.append('disgust_r')
+            cue_present.append(1)
+            type_of_trial.append('r')
+            stimulus_one.append('
+            
+        
+    
+    
+    
+    
+    
+    
     
 
--- emotional_faces_task/save_file_modification_function.py
+++ emotional_faces_task/save_file_modification_function.py
@@ -2,7 +2,7 @@ import openpyxl
 import os.path as op
 
 def save_file_modification(subject_id, total_blocks):
-    #data_path = 'data path here' 
+    data_path = 'data path here' 
     subject_id_file_name = subject_id + '_emotional_faces_data.xlsx'
     subject_id_metafile_name = subject_id + '_emotional_faces_meta_data.xlsx'
     subject_id_training_name = subject_id + '_emotional_faces_training_data.xlsx'
@@ -19,12 +19,12 @@ def save_file_modification(subject_id, total_blocks):
     block_number_list = [str(i+1) for i in range(total_blocks)] # list of strings from 1 to #total blocks
     block_number_strings = [('block_' + block_number_list[i]) for i in range(len(block_number_list))] # list of blocks
     
-    variable_strings = ['condition', 'cue_present', 'type_of_trial', 'stimulus_one', 'stimulus_two', 'systole_diastole', 'catch_trial', 'arrow_direction', 'correct_incorrect_late', 'iti']
+    variable_strings = ['condition', 'cue_present', 'type_of_trial', 'stimulus_one', 'stimulus_two', 'catch_trial', 'arrow_direction', 'correct_incorrect_late', 'iti']
 
     for ws_name in range(len(total_blocks)):
-        data_wb.create_sheet(faces_strings[ws_name]) # creating a sheet to save each block 
+        data_wb.create_sheet(block_number_strings[ws_name]) # creating a sheet to save each block 
         
-        data_sheet = data_wb[faces_strings[ws_name]]
+        data_sheet = data_wb[block_number_strings[ws_name]]
         
         for header in range(len(variable_strings)): # adding the headers
             data_sheet.cell(1, header+1, variable_strings[header])
@@ -33,7 +33,7 @@ def save_file_modification(subject_id, total_blocks):
 
     meta_variable_strings = ['subject_id', 'age', 'order', 'random_seed', 'session', 'colour_1', 'colour_2', 'colour_3'] # colours refer to the 
     meta_data_wb.create_sheet('meta_data')
-    meta_data_sheet = data_wb['meta_data')
+    meta_data_sheet = data_wb['meta_data']
 
     for header in range(len(meta_variable_strings)):
         meta_data_sheet.cell(1, header+1, meta_variable_strings[header])
@@ -47,3 +47,5 @@ def save_file_modification(subject_id, total_blocks):
         training_data_sheet.cell(1, header+1, variable_strings[header])
     
     return data_wb, meta_data_wb, training_data_wb, save_file, save_metafile, save_trainingfile
+
+
