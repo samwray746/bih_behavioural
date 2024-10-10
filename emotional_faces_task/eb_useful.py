9bb8837e34772b441544eeae12ba4e30949ba58d
samwray746 <S.Wray@bsms.ac.uk>
2024-10-10 12:09:14 +0100

    Update eb_useful.py

--- emotional_faces_task/eb_useful.py
+++ emotional_faces_task/eb_useful.py
@@ -8,42 +8,72 @@ def eb_useful(win, fch_w, fcv_w, fch_nw, fcv_nw, isi, iti, arrow_left, arrow_rig
     type_of_trial = [] # we know that if the cue is present it will be a repetition trial as this is a cue-useful block, but just adding for consistency 
     stimulus_one = [] # the first stimulus presented in the trial 
     stimulus_two = [] # the second stimulus presented in the trial 
+    face_record = [] # in what order was the different faces presented?
     catch = [] # was the trial a catch trial? 
     arrow_direction = [] # if it was a catch trial, what direction was the arrow pointing? 
     correct_incorrect = [] # was the response on the catch trial correct or incorrect 
     iti = [] # what was the inter-trial interval for the trial? 
     
-    # Setting up the structure of the experimental block. Some details: 
+    ### Setting up the structure of the experimental block. Some details: ###
         
     # 1) Each block consists of 40 trials i.e., the same number of actors we have in our stimulus set
     # 2) In each block, there are 20 repetition trials and 20 alternation trials
     # 3) In half of the 20 repetition/alternation trials a disgust face will be presented first, and in the other half a neutral face will be presented 
     # 4) In a cue useful block, whenever it's a repetition trial the fixation cross will always be non-white 
     
-    
     diff_poss_trials = ['disgust_r', 'disgust_a', 'neutral_r', 'neutral_a']
     
+    stim_set = [
+    [n_1, d_1], [n_2, d_2], [n_3, d_3], [n_4, d_4], [n_5, d_5],
+    [n_6, d_6], [n_7, d_7], [n_8, d_8], [n_9, d_9], [n_10, d_10],
+    [n_11, d_11], [n_12, d_12], [n_13, d_13], [n_14, d_14], [n_15, d_15],
+    [n_16, d_16], [n_17, d_17], [n_18, d_18], [n_19, d_19], [n_20, d_20],
+    [n_21, d_21], [n_22, d_22], [n_23, d_23], [n_24, d_24], [n_25, d_25],
+    [n_26, d_26], [n_27, d_27], [n_28, d_28], [n_29, d_29], [n_30, d_30],
+    [n_31, d_31], [n_32, d_32], [n_33, d_33], [n_34, d_34], [n_35, d_35],
+    [n_36, d_36], [n_37, d_37], [n_38, d_38], [n_39, d_39], [n_40, d_40]
+] # all the different stimuli that we will select from 
+    
     block_structure = [] 
+    prev_trial = 'first_trial' # creating now before we select trials 
     
     for trial in range(40): # creating the structure of each experimental block 
-        
-        if len(diff_poss_trials)== 0:
+        if len(diff_poss_trials)== 0: # resetting the list of possible trials 
             diff_poss_trials = ['disgust_r', 'disgust_a', 'neutral_r', 'neutral_a']
-        
-        
+    
         trial_pick = random.choice(diff_poss_trials)
+        
+        while True: # dealing with non-consecutive trials
+        if trial_pick == prev_trial:
+            trial_pick = random.choice(diff_poss_trials)
+        else: break 
+        
         diff_poss_trials.remove(trial_pick)
         block_structure.append(trial_pick)
+        face_record.append(random.choice(stim_set)) # selecting the sequence of faces randomly 
+
+    # Adding in catch trials. We want 20% (i.e., 8 trials per experimental block) to be catch trials 
+    
+    disgust_r_indices = [i for i, trial in enumerate(block_structure) if trial == 'disgust_r']
+    disgust_a_indices = [i for i, trial in enumerate(block_structure) if trial == 'disgust_a']
+    neutral_r_indices = [i for i, trial in enumerate(block_structure) if trial == 'neutral_r']
+    neutral_a_indices = [i for i, trial in enumerate(block_structure) if trial == 'neutral_a']
+
+    
+    
         
-    # appending the lists with relevant info 
+    # Appending the lists with relevant info 
     for trial in range(40):
-        
         condition_record.append('cue_useful')
         if block_structure[trial] == 'disgust_r': #So a repetition trial, and disgust is shown first
             condition_record.append('disgust_r')
             cue_present.append(1)
             type_of_trial.append('r')
-            stimulus_one.append('
+            stimulus_one.append(face_record[trial][1]) # i.e., the disgust stimulus
+            stimulus_two.append(face_record[trial][1]) # i.e., the disgust stimulus again 
+        
+
+
             
         
     
